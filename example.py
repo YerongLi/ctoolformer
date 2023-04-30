2cd443f00a8bac0e1d9745d2be536f57d01677af
Yerong <yerong.li@outlook.com>
2023-04-30 15:39:28 -0500

    set from_tf=True

--- example.py
+++ example.py
@@ -6,6 +6,7 @@ model = AutoModelForCausalLM.from_pretrained(
     r"dmayhem93/toolformer_v0_epoch2",
     torch_dtype=torch.float16,
     low_cpu_mem_usage=True,
+    from_tf=True
 ).cuda()
 generator = pipeline(
     "text-generation", model=model, tokenizer=tokenizer, device=0
